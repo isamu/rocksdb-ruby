0251727a1c427c9442aefccd7e907b6687648755
Isamu Arimoto <isamu.a@gmail.com>
2013-11-27 16:12:48 +0900

    fix typo of spec.

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -3,38 +3,37 @@ require "RocksDB"
 
 describe RocksDB do
   before do
-    @rockdb = RocksDB::DB.new "/tmp/file"
+    @rocksdb = RocksDB::DB.new "/tmp/file"
   end
 
   it 'should get data' do
-    @rockdb.put("test:read", "1")
-    @rockdb.get("test:read").should eq "1"
+    @rocksdb.put("test:read", "1")
+    @rocksdb.get("test:read").should eq "1"
   end
 
   it 'should put data' do 
-    @rockdb.put("test:put", "2").should be_true
-    @rockdb.get("test:put").should eq "2"
+    @rocksdb.put("test:put", "2").should be_true
+    @rocksdb.get("test:put").should eq "2"
   end
 
   it 'should delete data' do 
-    @rockdb.put("test:delete", "3")
-    @rockdb.get("test:delete").should eq "3"
+    @rocksdb.put("test:delete", "3")
+    @rocksdb.get("test:delete").should eq "3"
 
-    @rockdb.delete("test:delete").should be_true
-    @rockdb.get("test:delete").should be_empty
+    @rocksdb.delete("test:delete").should be_true
+    @rocksdb.get("test:delete").should be_empty
   end
 
   it 'should get multi data' do
-    @rockdb.put("test:multi1", "a")
-    @rockdb.put("test:multi2", "b")
-    @rockdb.put("test:multi3", "c")
+    @rocksdb.put("test:multi1", "a")
+    @rocksdb.put("test:multi2", "b")
+    @rocksdb.put("test:multi3", "c")
 
-    res = @rockdb.multi_get(["test:multi1", "test:multi2", "test:multi3"])
-    p res
+    @rocksdb.multi_get(["test:multi1", "test:multi2", "test:multi3"]).should eq ["a", "b", "c"]
   end
 
   after do
-    @rockdb.close
+    @rocksdb.close
   end
 
 end
