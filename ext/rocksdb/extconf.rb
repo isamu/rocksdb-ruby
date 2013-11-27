dc3d71ac1c8d0456d371e543083e91486b9dc449
Isamu Arimoto <isamu.a@gmail.com>
2013-11-27 09:19:54 +0900

    clean up extconf

--- ext/rocksdb/extconf.rb
+++ ext/rocksdb/extconf.rb
@@ -2,19 +2,9 @@ require "mkmf"
 
 dir_config('rocksdb')
 
-have_header('db.h')
-
-have_library('rocksdb')
-
-$LDFLAGS= " -lgflags -lz -lbz2"
-
-#$CCFLAGS= " -DROCKSDB_PLATFORM_POSIX  -DOS_MACOSX -DROCKSDB_ATOMIC_PRESENT -DGFLAGS -DZLIB -DBZIP2"
-$CPPFLAGS <<  " -std=gnu++11  -DROCKSDB_PLATFORM_POSIX  -DOS_MACOSX -DROCKSDB_ATOMIC_PRESENT -DGFLAGS -DZLIB -DBZIP2 "
-
-#SHARED_CFLAGS=" -fPIC"
-#SHARED_EXT=" dylib"
-#SHARED_LDFLAGS=" -dynamiclib -install_name "
-
-
-create_makefile("RocksDB")
-
+if have_header('db.h') and  have_library('rocksdb')
+  $CPPFLAGS << "-std=gnu++11"
+  create_makefile("RocksDB")
+else
+  abort "can't find header or library of rocksdb"
+end
