e5bf88b10bd9125552ae83b9e68c7a0f0b129fe3
Isamu Arimoto <isamu.a@gmail.com>
2013-11-27 09:52:35 +0900

    fix extconfy

--- ext/rocksdb/extconf.rb
+++ ext/rocksdb/extconf.rb
@@ -1,9 +1,10 @@
 require "mkmf"
 
 dir_config('rocksdb')
+RbConfig::CONFIG["CPP"] = "g++ -E -std=gnu++11"
 
-if have_header('db.h') and  have_library('rocksdb')
-  $CPPFLAGS << "-std=gnu++11"
+if have_header('rocksdb/db.h') and have_library('rocksdb') 
+  $CPPFLAGS << " -std=gnu++11"
   create_makefile("RocksDB")
 else
   abort "can't find header or library of rocksdb"
