6fa9afe525c3b288adcc21ba5f9fc1cd002e209b
Patrick Dinger <paxos2k@gmail.com>
2014-01-13 20:44:03 +0100

    Change makefile name from RocksDB to RocksDB/RocksDB to prevent runtime error

--- ext/rocksdb/extconf.rb
+++ ext/rocksdb/extconf.rb
@@ -5,7 +5,7 @@ RbConfig::CONFIG["CPP"] = "g++ -E -std=gnu++11"
 
 if have_header('rocksdb/db.h') and have_library('rocksdb') 
   $CPPFLAGS << " -std=gnu++11"
-  create_makefile("RocksDB")
+  create_makefile("RocksDB/RocksDB")
 else
   abort "can't find header or library of rocksdb"
 end
