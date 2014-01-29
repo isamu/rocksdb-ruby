30fd52909596f01d74dc2bf037229ed6571dedf0
Isamu Arimoto <isamu@to-kyo.to>
2014-01-30 05:02:43 +0900

    support readonly mode

---
+++ spec/db_readonly_spec.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require "rocksdb"
+
+describe RocksDB do
+  before do
+    @rocksdb = RocksDB::DB.new "/tmp/file2", {:readonly => true}
+  end
+
+  it 'should get data' do
+    @rocksdb.put("test:multi_db", "10")
+    expect(@rocksdb.get("test:multi_db")).to eq "1"
+  end
+
+  after do
+    @rocksdb.close
+  end
+end
