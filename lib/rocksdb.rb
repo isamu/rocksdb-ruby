3cee0f2f3d8a7a9fc1227253a7cc7151ce56c3ac
Isamu Arimoto <isamu@to-kyo.to>
2015-08-18 20:11:48 +0900

    use ruby's Enumerable #6

--- lib/rocksdb.rb
+++ lib/rocksdb.rb
@@ -3,14 +3,26 @@ require "rocksdb/ruby/version"
 
 module RocksDB
   class DB
+    include Enumerable
+    
     class << self
     end
-
+    
     alias :includes? :exists?
     alias :contains? :exists?
     alias :member? :exists?
     alias :[] :get
     alias :[]= :put
     alias :close! :close
+
+    def each(&block)
+      if block_given?
+        self.each_with_index do |key, value|
+          block.call(value)
+        end
+      else
+        self.iterator
+      end
+    end
   end
 end
