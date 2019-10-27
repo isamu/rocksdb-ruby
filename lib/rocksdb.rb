49e8ce73bcd95ca94e0c32fb2847e9860ecbde28
y8 <y8@ya.ru>
2019-10-27 16:54:46 +0300

    Delete `readonly` from Option Hash, to prevent it from being passed to RocksDB

--- lib/rocksdb.rb
+++ lib/rocksdb.rb
@@ -31,7 +31,7 @@ module RocksDB
       is_readonly = options[:readonly] || false
 
       if rocksdb_options.is_a? Hash
-        is_readonly = rocksdb_options[:readonly]
+        is_readonly = rocksdb_options.delete(:readonly)
 
         rocksdb_options = rocksdb_options.map do |key, value|
           [key, value].join("=")
