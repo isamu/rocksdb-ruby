1cdb652daf8e57385a0a51248c0d068d3e777fdd
y8 <y8@ya.ru>
2019-10-27 14:45:07 +0300

    Remove ununsed Iterator header signatures, re-arrange by function context

--- ext/rocksdb/rocksdb_iterator_rb.h
+++ ext/rocksdb/rocksdb_iterator_rb.h
@@ -9,12 +9,13 @@ extern "C" {
   rocksdb_iterator_pointer* get_iterator(VALUE *klass);
   rocksdb_iterator_pointer* get_iterator_for_read(VALUE *klass);
   void free_iterator(rocksdb_iterator_pointer* pointer);
+  VALUE rocksdb_iterator_alloc(VALUE klass);
 
+  VALUE rocksdb_iterator_seek(VALUE klass, VALUE v_target);
   VALUE rocksdb_iterator_seek_for_prev(VALUE klass, VALUE v_target);
   VALUE rocksdb_iterator_seek_to_first(VALUE klass);
   VALUE rocksdb_iterator_seek_to_last(VALUE klass);
-  VALUE rocksdb_iterator_seek(VALUE klass, VALUE v_target);
-  VALUE rocksdb_iterator_alloc(VALUE klass);
+
   VALUE rocksdb_iterator_valid(VALUE klass);
   VALUE rocksdb_iterator_key(VALUE klass);
   VALUE rocksdb_iterator_value(VALUE klass);
@@ -33,8 +34,5 @@ extern "C" {
 
   VALUE rocksdb_iterator_each_prefix(VALUE klass, VALUE v_prefix);
   VALUE rocksdb_iterator_each_range(VALUE klass, VALUE v_start, VALUE v_limit);
-
-  void iterate_each(rocksdb::Iterator* it);
-  void iterate_each_pair(rocksdb::Iterator* it);
 }
 
