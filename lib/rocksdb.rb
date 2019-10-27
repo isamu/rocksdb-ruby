c820005865413247a6d7f81479d1a6efa35dad86
y8 <y8@ya.ru>
2019-10-27 16:14:06 +0300

    Put back `0.2` methods to make current version backward compatible

--- lib/rocksdb.rb
+++ lib/rocksdb.rb
@@ -26,6 +26,7 @@ module RocksDB
 
   class DB
     extend Forwardable
+    extend Gem::Deprecate
 
     def initialize(path, rocksdb_options = "", options = {})
       is_readonly = options[:readonly] || false
@@ -54,13 +55,32 @@ module RocksDB
     alias_method :includes?, :exists?
     alias_method :contains?, :exists?
     alias_method :member?, :exists?
+    alias_method :member?, :exists?
     alias_method :[], :get
     alias_method :[]=, :put
     alias_method :close!, :close
 
-    def_delegators :new_iterator,
+    def_delegators :to_iterator,
                    :each, :reverse_each, :each_key, :reverse_each_key,
                    :each_pair, :reverse_each_pair,
                    :each_prefix, :each_range
+
+    alias_method :each_index, :each_key
+    alias_method :each_with_index, :each_pair
+
+    # Deprecated methods, kept for backward compatibility
+    alias_method :new_iterator, :to_iterator
+    deprecate :new_iterator, :to_iterator, 2019, 12
+
+    def is_readonly?
+      !writable?
+    end
+    deprecate :is_readonly?, :writable?, 2019, 12
+
+    alias_method :is_open?, :open?
+    deprecate :is_open?, :open?, 2019, 12
+
+    alias_method :multi_get, :get_many
+    deprecate :multi_get, :get_many, 2019, 12
   end
 end
