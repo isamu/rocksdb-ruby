879861ed8e17a7bcf3ccd5d20e9676b968903dba
Lars Volkhardt <lars.volkhardt@visable.com>
2022-03-23 11:38:55 +0100

    trying to fix ext compilation for rocksdb7

--- ext/rocksdb/extconf.rb
+++ ext/rocksdb/extconf.rb
@@ -1,12 +1,12 @@
 require "mkmf"
 
 dir_config('rocksdb')
-RbConfig::CONFIG["CPP"] = "g++ -E -std=gnu++11"
+RbConfig::CONFIG["CPP"] = "g++ -E -std=gnu++17"
 
 DEBUG_BUILD = have_library('rocksdb_debug') || ENV["DEBUG_LEVEL"]
 
 if have_header('rocksdb/db.h') and (have_library('rocksdb') or have_library('rocksdb_debug'))
-  $CPPFLAGS << " -std=gnu++11"
+  $CPPFLAGS << " -std=gnu++17"
 
   if DEBUG_BUILD
     CONFIG["optflags"] = "-O0"
